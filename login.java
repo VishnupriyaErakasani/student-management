d297dbede67100d5730b3e2e9f7734e5c472cce6
VishnupriyaErakasani <ekrishnaveni2005@gmail.com>
2025-09-17 10:23:16 +0530

    login details authentication while loggin to access details

--- login.java
+++ login.java
@@ -1,6 +1,6 @@
 public class login {
     public static void main(String[] args) {
-        System.out.println("This is login class");
+        System.out.println("This is login required to access student management system");
     }
     
 }
